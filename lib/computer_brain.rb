1a793c9aa020690a72500ebceef179190245dbea
Shane Galvin <Sgalvin36@gmail.com>
2024-07-22 13:12:04 -0600

    Add test for creating brain with a board parameter

--- lib/computer_brain.rb
+++ lib/computer_brain.rb
@@ -1,2 +1,7 @@
 class ComputerBrain
+
+    def initialize(player_board)
+        # @keys = player_board.cells.keys
+        # @board = player_board
+    end
 end
--- spec/computer_brain_spec.rb
+++ spec/computer_brain_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe ComputerBrain do
-    it 'exists' do
-        computer = ComputerBrain.new
-        expect(computer).to be_a ComputerBrain
+    before(:each) do
+        @player_board = Board.new
+        @cruiser = Ship.new("Cruiser", 3)
+        @submarine = Ship.new("Submarine", 2)
+        @player_board.place(@cruiser, ['A1', 'A2', 'A3'])
+        @player_board.place(@submarine, ['B2', 'B3'])
+        @computer = ComputerBrain.new(@player_board)
     end
+
+    it 'exists' do
+        expect(@computer).to be_a ComputerBrain
+    end
+
+    it 'initializes with access to player board' do
+    end
+
 end
